4f5e866f99f56eeca0b3352e4e47ecc2dbb6d234
Zach Gibbs <zacharywgibbs@gmail.com>
2024-07-01 15:50:13 -0500

    Created the addNoteDialog component to handle the opening and closing of the add note dialog pop up.

--- src/components/AddNoteDialog.tsx
+++ src/components/AddNoteDialog.tsx
@@ -1,1 +1,27 @@
-export default function AddNoteDialog() {}
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CreateNoteSchema, createNoteSchema } from "@/lib/validation/note";
+import { Dialog, DialogContent, DialogHeader } from "./ui/dialog";
+
+interface AddNoteDialogProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+export default function AddNoteDialog({ open, setOpen }: AddNoteDialogProps) {
+  const form = useForm<CreateNoteSchema>({
+    resolver: zodResolver(createNoteSchema),
+  });
+
+  async function onSubmit(input: CreateNoteSchema) {
+    alert(input);
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>Add Note</DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
