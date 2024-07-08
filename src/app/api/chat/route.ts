fe49902d87bf426e00bd9f51ff1be43dfe5e3831
Zach Gibbs <zacharywgibbs@gmail.com>
2024-07-08 12:58:23 -0500

    Set up ai chat to work and respond with answers in the chatbox.

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,9 @@
-import { getEmbedding } from "@/lib/openai";
+import { notesIndex } from "@/lib/db/pinecone";
+import openai, { getEmbedding } from "@/lib/openai";
 import { auth } from "@clerk/nextjs/server";
 import { ChatCompletionMessage } from "openai/resources/index.mjs";
+import prisma from "@/lib/db/prisma";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 export async function POST(req: Request) {
   try {
@@ -13,7 +16,7 @@ export async function POST(req: Request) {
     const messagesTruncated = messages.slice(-6);
 
     // generates the chat response by concatenating the content of the last 6 messages and passing it to the getEmbedding function.
-    const embeddings = await getEmbedding(
+    const embedding = await getEmbedding(
       messagesTruncated.map((message) => message.content).join("\n"),
     );
     //This is what we turn into a vector embedding
@@ -22,6 +25,44 @@ export async function POST(req: Request) {
     // Thanks! What is my phone pin?
 
     const { userId } = auth();
+
+    const vectorQueryResponse = await notesIndex.query({
+      vector: embedding,
+      // topK is set how many notes you want to return.
+      // if you make this 3, it will return the top 3 most similar notes.
+      // The higher the number, the more notes you will get back and more processing time/token usage it will take.
+      topK: 4,
+      filter: { userId },
+    });
+
+    const relevantNotes = await prisma.note.findMany({
+      where: {
+        id: {
+          in: vectorQueryResponse.matches.map((match) => match.id),
+        },
+      },
+    });
+
+    console.log("Relevant notes found:", relevantNotes);
+
+    const systemMessage: ChatCompletionMessage = {
+      role: "system",
+      content:
+        "You are an intelligent note-taking app. You answer the user's question based on their existing notes. " +
+        "The relevan notes for this query are:\n" +
+        relevantNotes
+          .map((note) => `Title: ${note.title}\n\nContent:\n${note.content}`)
+          .join("\n\n"),
+    };
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages: [systemMessage, ...messagesTruncated],
+    });
+
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
--- src/components/AIChatBox.tsx
+++ src/components/AIChatBox.tsx
@@ -3,6 +3,7 @@ import { useChat } from "ai/react";
 import { XCircle } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { Message } from "ai";
 
 interface AIChatBoxProps {
   open: boolean;
@@ -31,7 +32,11 @@ export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
         <XCircle size={30} />
       </button>
       <div className="flex h-[600px] flex-col rounded border-2 bg-background shadow-2xl shadow-gray-400">
-        <div className="h-full">Messages</div>
+        <div className="h-full">
+          {messages.map((message) => (
+            <ChatMessage message={message} key={message.id} />
+          ))}
+        </div>
         <form action="" onSubmit={handleSubmit} className="m-3 flex gap-1">
           <Input
             value={input}
@@ -41,6 +46,15 @@ export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
           <Button type="submit">Send</Button>
         </form>
       </div>
+    </div>
+  );
+}
+
+function ChatMessage({ message: { role, content } }: { message: Message }) {
+  return (
+    <div className="mb-3">
+      <div>{role}</div>
+      <div>{content}</div>
     </div>
   );
 }
